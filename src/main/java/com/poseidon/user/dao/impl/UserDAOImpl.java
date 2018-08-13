25c9a3c205583da0c115bdc3a85d384b298f4864
Suraj Muraleedharan <v-smuraleedharan@expedia.com>
2018-08-13 22:46:01 +0530

    full hibernate implementation on user path

--- src/main/java/com/poseidon/user/dao/impl/UserDAOImpl.java
+++ src/main/java/com/poseidon/user/dao/impl/UserDAOImpl.java
@@ -4,24 +4,27 @@ import com.poseidon.user.dao.UserDAO;
 import com.poseidon.user.dao.entities.User;
 import com.poseidon.user.domain.UserVO;
 import com.poseidon.user.exception.UserException;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 /**
  * @author : Suraj Muraleedharan
- *         Date: Nov 27, 2010
- *         Time: 12:43:13 PM
+ * Date: Nov 27, 2010
+ * Time: 12:43:13 PM
  */
 @Repository
 @SuppressWarnings("unused")
@@ -29,12 +32,10 @@ public class UserDAOImpl implements UserDAO {
     private static final Logger LOG = LoggerFactory.getLogger(UserDAOImpl.class);
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
     private UserRepository userRepository;
 
-    private static final String SEARCH_USERS_SQL = " select * from user ";
+    @PersistenceContext
+    private EntityManager em;
 
     /**
      * LOG in
@@ -53,11 +54,11 @@ public class UserDAOImpl implements UserDAO {
         }
 
         if (user != null) {
-            LOG.info(" user details fetched successfully,for user name {}" , user.getName());
+            LOG.info(" user details fetched successfully,for user name {}", user.getName());
             if (!userVO.getPassword().equalsIgnoreCase(user.getPassword())) {
                 throw new UserException(UserException.INCORRECT_PASSWORD);
             }
-            LOG.info(" Password matched successfully, user details are {}" , user);
+            LOG.info(" Password matched successfully, user details are {}", user);
         } else {
             // invalid user
             throw new UserException(UserException.UNKNOWN_USER);
@@ -167,53 +168,45 @@ public class UserDAOImpl implements UserDAO {
      * @throws DataAccessException on error
      */
     @SuppressWarnings("unchecked")
-    private List<UserVO> searchAllUsers(UserVO searchUser) throws DataAccessException {
-        StringBuilder dynamicQuery = new StringBuilder(SEARCH_USERS_SQL);
-        Boolean isWhereAppended = Boolean.FALSE;
-        if (searchUser.getName() != null && searchUser.getName().trim().length() > 0) {
-            dynamicQuery.append(" where ");
-            isWhereAppended = Boolean.TRUE;
-            if (searchUser.getIncludes()) {
-                dynamicQuery.append(" name like '%").append(searchUser.getName()).append("%'");
-            } else if (searchUser.getStartsWith()) {
-                dynamicQuery.append(" name like '").append(searchUser.getName()).append("%'");
-            } else {
-                dynamicQuery.append(" name like '").append(searchUser.getName()).append("'");
+    private List<UserVO> searchAllUsers(UserVO searchUser) {
+        CriteriaBuilder builder = em.unwrap(Session.class).getCriteriaBuilder();
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        Root<User> userRoot = criteria.from(User.class);
+        criteria.select(userRoot);
+
+        if (searchUser.getIncludes()) {
+            if (!StringUtils.isEmpty(searchUser.getName())) {
+                criteria.where(builder.like(userRoot.get("name"), "%" + searchUser.getName() + "%"));
+            }
+            if (!StringUtils.isEmpty(searchUser.getLoginId())) {
+                criteria.where(builder.like(userRoot.get("logInId"), "%" + searchUser.getLoginId() + "%"));
+            }
+            if (!StringUtils.isEmpty(searchUser.getRole())) {
+                criteria.where(builder.like(userRoot.get("role"), "%" + searchUser.getRole() + "%"));
+            }
+        } else if (searchUser.getStartsWith()) {
+            if (!StringUtils.isEmpty(searchUser.getName())) {
+                criteria.where(builder.like(userRoot.get("name"), searchUser.getName() + "%"));
+            }
+            if (!StringUtils.isEmpty(searchUser.getLoginId())) {
+                criteria.where(builder.like(userRoot.get("logInId"), searchUser.getLoginId() + "%"));
+            }
+            if (!StringUtils.isEmpty(searchUser.getRole())) {
+                criteria.where(builder.like(userRoot.get("role"), searchUser.getRole() + "%"));
+            }
+        } else {
+            if (!StringUtils.isEmpty(searchUser.getName())) {
+                criteria.where(builder.equal(userRoot.get("name"), searchUser.getName()));
+            }
+            if (!StringUtils.isEmpty(searchUser.getLoginId())) {
+                criteria.where(builder.equal(userRoot.get("logInId"), searchUser.getLoginId()));
+            }
+            if (!StringUtils.isEmpty(searchUser.getRole())) {
+                criteria.where(builder.equal(userRoot.get("role"), searchUser.getRole()));
             }
         }
-
-        if (searchUser.getLoginId() != null && searchUser.getLoginId().trim().length() > 0) {
-            if (!isWhereAppended) {
-                dynamicQuery.append(" where ");
-            } else {
-                dynamicQuery.append(" and ");
-            }
-            if (searchUser.getIncludes()) {
-                dynamicQuery.append(" logInId like '%").append(searchUser.getLoginId()).append("%'");
-            } else if (searchUser.getStartsWith()) {
-                dynamicQuery.append(" logInId like '").append(searchUser.getLoginId()).append("%'");
-            } else {
-                dynamicQuery.append(" logInId like '").append(searchUser.getLoginId()).append("'");
-            }
-        }
-
-        if (searchUser.getRole() != null && searchUser.getRole().length() > 0) {
-            if (!isWhereAppended) {
-                dynamicQuery.append(" where ");
-            } else {
-                dynamicQuery.append(" and ");
-            }
-            if (searchUser.getIncludes()) {
-                dynamicQuery.append(" role like '%").append(searchUser.getRole()).append("%'");
-            } else if (searchUser.getStartsWith()) {
-                dynamicQuery.append(" role like '").append(searchUser.getRole()).append("%'");
-            } else {
-                dynamicQuery.append(" role like '").append(searchUser.getRole()).append("'");
-            }
-        }
-
-        LOG.info("Query generated is " + dynamicQuery);
-        return (List<UserVO>) jdbcTemplate.query(dynamicQuery.toString(), new UserRowMapper());
+        List<User> resultUsers = em.unwrap(Session.class).createQuery(criteria).getResultList();
+        return convertUsersToUserVOs(resultUsers);
     }
 
     /**
@@ -273,33 +266,5 @@ public class UserDAOImpl implements UserDAO {
     public UserVO findByUsername(String username) {
         User user = userRepository.findByName(username);
         return convertTOUserVO(user);
-    }
-
-    /**
-     * Row mapper as inner class
-     */
-    private class UserRowMapper implements RowMapper {
-
-        /**
-         * method to map the result to vo
-         *
-         * @param resultSet resultSet instance
-         * @param i         i instance
-         * @return UserVO as Object
-         * @throws SQLException on error
-         */
-        public Object mapRow(ResultSet resultSet, int i) throws SQLException {
-            UserVO user = new UserVO();
-            user.setId(resultSet.getLong("id"));
-            user.setName(resultSet.getString("name"));
-            user.setLoginId(resultSet.getString("logInId"));
-            user.setPassword(resultSet.getString("password"));
-            user.setRole(resultSet.getString("role"));
-            user.setCreatedBy(resultSet.getString("createdBy"));
-            user.setCreatedDate(resultSet.getDate("createdOn"));
-            user.setLastModifiedBy(resultSet.getString("modifiedBy"));
-            user.setModifiedDate(resultSet.getDate("modifiedOn"));
-            return user;
-        }
     }
 }
