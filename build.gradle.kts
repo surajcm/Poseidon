15291eb4a264337188fc0e9559bb4829541cc4dc
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2019-12-08 07:48:49 +0530

    updating gradle and removing all the warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.2.1.RELEASE")
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.2.2.RELEASE")
         classpath("gradle.plugin.com.github.spotbugs:spotbugs-gradle-plugin:2.0.0")
     }
 }
@@ -13,7 +13,7 @@ plugins {
     java
     idea
     application
-    id("org.springframework.boot") version "2.2.1.RELEASE"
+    id("org.springframework.boot") version "2.2.2.RELEASE"
     id("net.ltgt.errorprone") version "0.8.1"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
@@ -23,17 +23,20 @@ apply("gradle/staticCodeAnalysis.gradle")
 
 //println org.gradle.internal.jvm.Jvm.current()
 
-tasks.jar {
-    baseName = "poseidon"
-    version = "0.0.1-SNAPSHOT"
+val jar by tasks.getting(Jar::class) {
+    archiveBaseName.set("poseidon")
+    archiveVersion.set("0.0.1-SNAPSHOT")
 }
+
 application {
     applicationDefaultJvmArgs = listOf("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005")
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_13
     targetCompatibility = JavaVersion.VERSION_13
 }
+
 repositories {
     mavenLocal()
     mavenCentral()
