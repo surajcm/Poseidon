039d1900afc74c289dfc6d67b3ebce0e378aa98f
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-05-01 19:14:12 +0530

    adding more test cases

--- src/test/java/com/poseidon/company/dao/impl/CompanyTermsDAOImplTest.java
+++ src/test/java/com/poseidon/company/dao/impl/CompanyTermsDAOImplTest.java
@@ -1,6 +1,7 @@
 package com.poseidon.company.dao.impl;
 
 import com.poseidon.company.dao.entities.CompanyTerms;
+import com.poseidon.company.domain.CompanyTermsVO;
 import com.poseidon.company.exception.CompanyTermsException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,11 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.powermock.reflect.Whitebox;
+import org.springframework.dao.CannotAcquireLockException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -31,11 +36,51 @@ public class CompanyTermsDAOImplTest {
         Assertions.assertNotNull(companyTermsDAO.listCompanyTerms());
     }
 
+    @Test
+    public void listCompanyTermsFailure() throws CompanyTermsException {
+        when(companyTermsRepository.findAll()).thenThrow(new CannotAcquireLockException("DB error"));
+        Assertions.assertThrows(CompanyTermsException.class, companyTermsDAO::listCompanyTerms);
+    }
+
+    @Test
+    public void updateCompanyDetailsSuccessWithNull() throws CompanyTermsException {
+        when(companyTermsRepository.findById(anyLong())).thenReturn(Optional.empty());
+        Assertions.assertNull(companyTermsDAO.updateCompanyDetails(new CompanyTermsVO()));
+    }
+
+    @Test
+    public void updateCompanyDetailsSuccessWithNotNull() throws CompanyTermsException {
+        when(companyTermsRepository.findById(anyLong())).thenReturn(Optional.of(mockCompanyTerms()));
+        when(companyTermsRepository.save(any())).thenReturn(mockCompanyTerms());
+        Assertions.assertNotNull(companyTermsDAO.updateCompanyDetails(new CompanyTermsVO()));
+    }
+
+    @Test
+    public void updateCompanyDetailsFailure() throws CompanyTermsException {
+        when(companyTermsRepository.findById(anyLong())).thenThrow(new CannotAcquireLockException("DB error"));
+        Assertions.assertThrows(CompanyTermsException.class,
+                () -> companyTermsDAO.updateCompanyDetails(new CompanyTermsVO()));
+    }
+
     private List<CompanyTerms> mockCompanyTermsList() {
         CompanyTerms companyTerms = new CompanyTerms();
         List<CompanyTerms> companyTermsList = new ArrayList<>();
         companyTermsList.add(companyTerms);
         return companyTermsList;
+    }
+
+    private CompanyTerms mockCompanyTerms() {
+        CompanyTerms companyTerms = new CompanyTerms();
+        companyTerms.setCompanyName("ABC");
+        companyTerms.setTerms("ABC");
+        companyTerms.setCompanyAddress("ABC");
+        companyTerms.setCompanyPhone("ABC");
+        companyTerms.setCompanyEmail("ABC");
+        companyTerms.setCompanyWebsite("ABC");
+        companyTerms.setVatTin("ABC");
+        companyTerms.setCstTin("ABC");
+        companyTerms.setModifiedBy("ABC");
+        return companyTerms;
     }
 
 }
