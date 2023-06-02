77bc91f00b5c3cf35ca7a92edc5944eacd722af3
Suraj Muraleedharan <surajcm@gmail.com>
2023-06-02 16:24:49 +0530

    Upgrading to java 20

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-19.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-20.jdk/Contents/Home"
 export JAVA_OPTS="-Xms512m -Xmx512m"
 # java -Dserver.port=8181 $JAVA_OPTS -jar build/libs/*.jar
 exec ./gradlew "$@"
